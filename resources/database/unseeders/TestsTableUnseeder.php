38054e1a8aef49d4617f2571cce32ce90965b3cc
Mark Topper <mark@ulties.com>
2018-01-19 19:59:34 +0100

    Update TestsTableUnseeder.php

--- resources/database/unseeders/TestsTableUnseeder.php
+++ resources/database/unseeders/TestsTableUnseeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class TestsTableSeeder extends Seeder
+class TestsTableUnseeder extends Seeder
 {
     /**
      * Run the database seeds.
